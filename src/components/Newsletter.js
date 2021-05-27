6cbe3fd0f187ba9ab5a441df96f04fda43132f9b
twilightscapes <ihatespam@gmail.com>
2021-05-27 07:02:55 -0500

    newsletter try 2

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -21,11 +21,13 @@ const NewsletterPage = () => (
           data-netlify="true"
           data-netlify-honeypot="bot-field">
 
+            <div style={{background:'#333', margin:'0 20px', padding:'10px 0', borderRadius:'12px'}}>
+
           <input type="hidden" name="form-name" value="news" />
 
 
             <label style={{color:'#fff'}}>
-              Join the Night Newsletter - (it's free and no SPAM!)<br />
+              Join the Night Newsletter - (it's free and no SPAM!)<br /><br />
               <input type="email" name="email" required={false}
                       placeholder="your@email.com"
                       style={{margin: '0 1rem 0 0', padding:'.6rem .8rem', width: '50%', border:'2px solid #000', borderRadius:'8px'}}/>
@@ -46,7 +48,7 @@ const NewsletterPage = () => (
                 <RiSendPlane2Line />
               </span>
             </button>
-
+</div>
 
         </form>
 
